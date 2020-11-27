2e767a7a26f0e1f3922be138a4c11bae5dfcd948
dvstepanchuk <danylo.stepanchuk@globallogic.com>
2020-11-27 08:34:54 +0200

    HMR: Added python wc

--- labs/hadoop-mr/word_counter_python/mapper.py
+++ labs/hadoop-mr/word_counter_python/mapper.py
@@ -2,5 +2,5 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    words = '\n'.join(['\t'.join([w.strip(), '1']) for w in line.split(" ")])
-    print(words)
+    # words = '\n'.join(['\t'.join([w.strip(), '1']) for w in line.split(" ")])
+    print('x\t1')
