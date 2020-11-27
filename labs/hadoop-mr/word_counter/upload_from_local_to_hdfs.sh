05de93a0cbb72d3ef54901b9da2cc724429f9637
dvstepanchuk <danylo.stepanchuk@globallogic.com>
2020-11-27 06:57:08 +0200

    HMR: FIxed upload_from_local_to_hdfs.sh

--- labs/hadoop-mr/word_counter/upload_from_local_to_hdfs.sh
+++ labs/hadoop-mr/word_counter/upload_from_local_to_hdfs.sh
@@ -29,7 +29,7 @@ BASE_PATH=$(readlink -f "$THIS_PATH/../")
 APP_PATH="$THIS_PATH/word_counter-1.0-jar-with-dependencies.jar"
 
 if [ "${FORMAT}" = 'txt' ]; then
-  LOCAL_PATH="${BASE_PATH}/../data/word_count"
+  LOCAL_PATH="${BASE_PATH}/data/word_count"
 else
   LOCAL_PATH="${BASE_PATH}/data/word_count_gzip"
 fi
