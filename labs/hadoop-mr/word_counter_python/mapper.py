85bdd904c7cee6c47f72729beda94473604d66ba
dvstepanchuk <danylo.stepanchuk@globallogic.com>
2020-11-27 08:36:55 +0200

    HMR: Added python wc

--- labs/hadoop-mr/word_counter_python/mapper.py
+++ labs/hadoop-mr/word_counter_python/mapper.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
 import sys
 
+# input comes from STDIN (standard input)
 for line in sys.stdin:
+    # remove leading and trailing whitespace
     line = line.strip()
-    # words = '\n'.join(['\t'.join([w.strip(), '1']) for w in line.split(" ")])
-    print('x\t1')
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
